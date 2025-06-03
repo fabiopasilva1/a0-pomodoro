6019ea3025430580ab462b969d7e8db3923e860e
fabiopasilva1 <fabiopasilva1@gmail.com>
2025-06-03 01:32:14 -0300

    Input: um componente um pouco mais complexo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -13,6 +15,27 @@ export function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
