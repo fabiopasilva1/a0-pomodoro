62d79c1d9a91328d53995bf2e6574d6985239346
fabiopasilva1 <fabiopasilva1@gmail.com>
2025-06-02 23:59:20 -0300

    S2A14/Import CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { Heading } from './components/Heading/Heading';
+import './styles/global.css';
 export function App() {
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, quasi.
-        Molestiae, saepe! Voluptatum, debitis culpa quidem quas illum deleniti
-        mollitia, pariatur sequi, dignissimos praesentium rem ab id vero
-        dolorem. Ad!
-      </p>
+      <Heading />
     </>
   );
 }
---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,14 @@
+import styles from './Heading.module.css';
+export function Heading() {
+  return (
+    <>
+      <h1 className={styles.heading}>Olá Mundo!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, quasi.
+        Molestiae, saepe! Voluptatum, debitis culpa quidem quas illum deleniti
+        mollitia, pariatur sequi, dignissimos praesentium rem ab id vero
+        dolorem. Ad!
+      </p>
+    </>
+  );
+}
