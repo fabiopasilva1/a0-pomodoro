1b40fe0344ad6f52459e143e311b2361cb8bcdb0
fabiopasilva1 <fabiopasilva1@gmail.com>
2025-06-03 01:54:47 -0300

    Input: adicionando estilos de CSS para input ativo, em foco e desativado

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,12 @@ export function App() {
       <Container>
         <form className='form'>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='task'
+              placeholder='Digite uma tarefa'
+            />
           </div>
 
           <div className='formRow'>
