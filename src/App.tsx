c617157ec22e11d948e88276d01c4526d6a8ea09
fabiopasilva1 <fabiopasilva1@gmail.com>
2025-06-02 23:40:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, quasi.
+        Molestiae, saepe! Voluptatum, debitis culpa quidem quas illum deleniti
+        mollitia, pariatur sequi, dignissimos praesentium rem ab id vero
+        dolorem. Ad!
+      </p>
+    </>
+  );
+}
