8a6af37b71fec50f63e30e27dbd53c6b5ce1b483
fabiopasilva1 <fabiopasilva1@gmail.com>
2025-08-30 23:19:08 -0300

    fix: ajusta formatação e estrutura de arquivos para melhor legibilidade e consistência

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const isPages = mode === 'pages'
-  return {
-    base: isPages ? '/a0-pomodoro/' : '/',
-    plugins: [react()],
-  }
+    const isPages = mode === 'pages'
+    return {
+        base: isPages ? '/a0-pomodoro/' : '/',
+        plugins: [react()],
+    }
 })
